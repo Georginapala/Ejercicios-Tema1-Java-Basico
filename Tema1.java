8b481ece332d4a673fa69f9d92bac8ba55777cc0
Georgina <91339786+Georginapala@users.noreply.github.com>
2022-06-05 22:49:10 -0300

    Add files via upload

---
+++ Tema1.java
@@ -0,0 +1,13 @@
+package com.Openbootcamp;
+
+public class Tema1 {
+    public static void main(String[] args) {
+        System.out.println("TIPOS DE DATOS");
+
+        System.out.println("1) Numericos:");
+        System.out.println("1.1- Enteros: byte, short, int, long.El mas utilizado es el int.");
+        System.out.println("1.2- Decimales: float, double. Su valor es decimal, y la coma, se marca con un punto.");
+        System.out.println("2) Booleano: su valor es true or false.\n   Son datos logicos, por ejemplo and && y or ||.");
+        System.out.println("3) Texto: char, String.\n   El char es para un solo caracter.\n   El cual puede ser un simbolo o una letra y se escribe entre comillas simples.\n   El String puede ser una palabra o un numero o simbolo, pero siempre va entre comillas dobles.");
+    }
+}
